433cc8fc30d09352125d491c15bc64fb612a08ae
John Denny <johnnydenny@gmail.com>
2022-04-04 15:22:02 -0400

    fixed like button animation

--- src/components/ProfileImagesLoader.js
+++ src/components/ProfileImagesLoader.js
@@ -28,6 +28,7 @@ const ProfileImagesLoader = (props) => {
   const [movementStart, setMovementStart] = useState('');
   const [isMoving, setIsMoving] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
+  const [isButtonLiked, setIsButtonLiked] = useState(false);
 
   const onDoublePress = (index, event) => {
     const time = new Date().getTime();
@@ -65,6 +66,9 @@ const ProfileImagesLoader = (props) => {
     const { postID } = photosArray[index][0];   
     const alreadyLiked = photosArray[index][0].likes.findIndex((like) => like.uid === uid);
     if (alreadyLiked === -1) {
+      const newIsButtonLiked = [...isButtonLiked];
+      newIsButtonLiked.splice(index, 1, true);
+      setIsButtonLiked(newIsButtonLiked);
       const newLikes = [...photosArray[index][0].likes];
       const newPost = {...photosArray[index][0], likes: newLikes};
       const newArray = [...photosArray[index]];
@@ -81,6 +85,9 @@ const ProfileImagesLoader = (props) => {
       fullPost.splice(index, 1, newArray);
       setPhotosArray(fullPost);      
     } else {
+      const newIsButtonLiked = [...isButtonLiked];
+      newIsButtonLiked.splice(index, 1, false);
+      setIsButtonLiked(newIsButtonLiked);
       const newLikes = [...photosArray[index][0].likes];
       const newPost = {...photosArray[index][0], likes: newLikes};
       const newArray = [...photosArray[index]];
@@ -169,6 +176,7 @@ const ProfileImagesLoader = (props) => {
     setGalleryIndex(new Array(12).fill(0));
     setMovement(new Array(12).fill(0));
     setIsLiked(new Array(12).fill(false));
+    setIsButtonLiked(new Array(12).fill(false));
   },[]);
 
   useEffect(() => {
@@ -395,7 +403,7 @@ const ProfileImagesLoader = (props) => {
                         </svg>                      
                       }
                       {liked !== -1 &&
-                        <svg aria-label="Unlike" className="like-filled-svg" color="#ed4956" fill="#ed4956" height="24" role="img" viewBox="0 0 48 48" width="24">
+                        <svg aria-label="Unlike" className={isButtonLiked[postIndex] ? "like-filled-svg animated" : 'like-filled-svg'} color="#ed4956" fill="#ed4956" height="24" role="img" viewBox="0 0 48 48" width="24">
                           <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
                         </svg>                      
                       }
